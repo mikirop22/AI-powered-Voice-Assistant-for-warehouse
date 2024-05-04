7d9c85b66f92ac98432bda5a62a0709f6a231769
Llucfurriols <jocslluc7@gmail.com>
2024-05-04 18:20:36 +0200

    proba custom_recognisor

--- recognisor_micro.py
+++ recognisor_micro.py
@@ -1,13 +1,37 @@
 import speech_recognition as sr
+import numpy as np
+import librosa
 
+# Cargar el modelo entrenado
+from tensorflow.keras.models import load_model
+
+model = load_model("model.h5")
 # Inicializar el reconocedor de voz
 recognizer = sr.Recognizer()
 
 # Función para reconocer la siguiente palabra con el modelo entrenado
 def recognize_custom(audio):
+    # Convertir el audio capturado por el micrófono en características (MFCC)
+    mfccs = extract_features_from_audio(audio)
+    
+    
+    # Predecir utilizando tu modelo entrenado
+    prediction = model.predict_classes(mfccs)
+    
+    # Imprimir la palabra predicha (o hacer lo que desees con la predicción)
+    print("Palabra predicha con el modelo entrenado:", prediction)
+
+# Función para extraer características (MFCC) del audio
+def extract_features_from_audio(audio):
+    # Convertir el audio a matriz numérica
+    audio_data = np.frombuffer(audio.frame_data, dtype=np.int16)
+    
+    # Extraer características (MFCC) del audio
+    mfccs = librosa.feature.mfcc(y=audio_data, sr=audio.sample_rate, n_mfcc=13)
+    return mfccs
 
 
-    print("Palabra reconocida con el modelo entrenado: {}".format(audio))
+
 
 
 
@@ -23,14 +47,16 @@ while True:
             print("Dijiste: {}".format(text))
             
             # Si se detecta "salir", se rompe el bucle
-            if text.lower() == "salir":
+            if "salir" in text.lower():
                 break
                 
             # Si se detecta "añadir", se espera la siguiente palabra
-            if text.lower() == "añadir":
+            if "añadir" in text.lower():
                 print("Escuchando siguiente palabra...")
                 audio = recognizer.listen(mic)
                 recognize_custom(audio)
+                print("Palabra añadida.")
+            
                 
         except sr.UnknownValueError:
             print("Lo siento, no pude entender el audio.")
