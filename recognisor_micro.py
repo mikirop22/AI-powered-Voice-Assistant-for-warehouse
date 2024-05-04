48be65be0e25dcde6f11738f1e42e4b8de336edf
janbennassar <147800871+janbennassar@users.noreply.github.com>
2024-05-05 01:58:21 +0200

    hgsfas

--- recognisor_micro.py
+++ recognisor_micro.py
@@ -116,4 +116,24 @@ while True:
         except sr.RequestError as e:
             print("Error ocurrido; {0}".format(e))
 
-# La llista generada s'ha de guardar i pujar d'alguna manera al núbol, per tal que es pugui accedir de forma fàcil desde la terminal del treballador del magatzem
+
+
+# ENVIAR LA LLISTA A GOOGLE DIRVE, per tal que es pugui accedir de forma fàcil desde la terminal del treballador del magatzem
+from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials
+from googleapiclient.http import MediaFileUpload
+
+# Define las credenciales
+credentials = Credentials.from_service_account_file('subtle-circlet-422322-b5-50795e26a89a.json')
+
+# Autentica con las credenciales
+drive_service = build('drive', 'v3', credentials=credentials)
+
+# Carga el archivo 'list.csv' a Google Drive
+file_metadata = {'name': 'list.csv'}
+media = MediaFileUpload('list.csv', mimetype='text/csv')
+file = drive_service.files().create(body=file_metadata,
+                                    media_body=media,
+                                    fields='id').execute()
+
+print('Archivo "list.csv" cargado correctamente con el ID:', file.get('id'))
