cabbde054292e164c3fcd25b49643a93288face6
janbennassar <147800871+janbennassar@users.noreply.github.com>
2024-05-05 05:10:42 +0200

    jhgd

--- transmissió/rebre.py
+++ transmissió/rebre.py
@@ -1,3 +1,4 @@
+import os
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 from googleapiclient.http import MediaIoBaseDownload
@@ -8,16 +9,23 @@ credentials = Credentials.from_service_account_file('subtle-circlet-422322-b5-50
 # Autentica con las credenciales
 drive_service = build('drive', 'v3', credentials=credentials)
 
-# Solicita al usuario que ingrese el ID del archivo 'list.csv'
-file_id = input("Por favor, ingresa el ID de la lista: ")
+# Solicita al usuario que ingrese el nombre e ID del archivo
+list_name = input("Por favor, ingresa el nombre de la lista: ")
+list_id = input("Por favor, ingresa el ID de la lista: ")
 
-# Descarga el archivo 'list.csv' de Google Drive
-request = drive_service.files().get_media(fileId=file_id)
-fh = open('list.csv', 'wb')
-downloader = MediaIoBaseDownload(fh, request)
-done = False
-while not done:
-    status, done = downloader.next_chunk()
+# Directorio donde deseas guardar el archivo
+directory = 'transmissió/'
 
-print('Archivo "list.csv" descargado correctamente.')
+# Ruta completa del archivo con el nombre proporcionado por el usuario
+file_path = os.path.join(directory, f'{list_name}.csv')
+
+# Descarga el archivo con el nombre proporcionado por el usuario desde Google Drive
+request = drive_service.files().get_media(fileId=list_id)
+with open(file_path, 'wb') as fh:
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while not done:
+        status, done = downloader.next_chunk()
+
+print(f'Archivo "{list_name}.csv" descargado correctamente.')
 
