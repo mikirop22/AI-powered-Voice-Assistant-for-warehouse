a0c9f09ee1e797657d2061e253248a0321fe62ba
janbennassar <147800871+janbennassar@users.noreply.github.com>
2024-05-05 02:04:05 +0200

    GFJGHD

--- pygame/main_allistar.py
+++ pygame/main_allistar.py
@@ -1,5 +1,29 @@
 import csv
 from magatzem import Warehouse
+from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials
+from googleapiclient.http import MediaIoBaseDownload
+
+#DESCARREGAR LA LLISTA
+# Define las credenciales
+credentials = Credentials.from_service_account_file('subtle-circlet-422322-b5-50795e26a89a.json')
+
+# Autentica con las credenciales
+drive_service = build('drive', 'v3', credentials=credentials)
+
+# Solicita al usuario que ingrese el ID del archivo 'list.csv'
+file_id = input("Por favor, ingresa el ID de la lista: ")
+
+# Descarga el archivo 'list.csv' de Google Drive
+request = drive_service.files().get_media(fileId=file_id)
+fh = open('list.csv', 'wb')
+downloader = MediaIoBaseDownload(fh, request)
+done = False
+while not done:
+    status, done = downloader.next_chunk()
+
+print('Archivo "list.csv" descargado correctamente.')
+
 
 magatzem = [[[0, None, 0] for _ in range(10)] for _ in range(10)]
 for fila in magatzem:
