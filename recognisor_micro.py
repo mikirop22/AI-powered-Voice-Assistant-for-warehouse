e87c25ac43af3785f070bfde5ab63795763db4fa
Llucfurriols <jocslluc7@gmail.com>
2024-05-05 02:21:06 +0200

    tinc son

--- recognisor_micro.py
+++ recognisor_micro.py
@@ -38,6 +38,14 @@ def agregar_producto(name, input_csv, output_csv):
     print(f"Se ha añadido el elementos con el nombre '{name}' en el archivo '{output_csv}'.")
 
 
+# Definir el mapeo entre palabras técnicas y palabras reconocidas por Google
+mapeo_palabras = {
+    "OTINET 125mililitros": ["otinet 125 ml"],
+    "DEPO MODERIN 5mililitros": ["depo modern 5 ml", "pepo modern 5 ml", "Depo mothering 5 ml"],
+    "VETREGUL GEL 50mililitros": ["vertegui gel 50 ml"]
+}
+
+
 while True:
     with sr.Microphone() as mic:
         print("Di algo...")
@@ -57,21 +65,46 @@ while True:
             if "añadir" in text.lower():
                 print("Escuchando siguiente palabra...")
                 audio = recognizer.listen(mic, timeout=None)
+                text = recognizer.recognize_google(audio, language="es-ES")
                 print("Grabación finalizada.")
+                print("Dijiste: {}".format(text))
 
-                # Guardar el audio en formato WAV
-                with open("audio_temp.wav", "wb") as f:
-                    f.write(audio.get_wav_data())
-                
-                # Convertir el audio a formato MP3
-                sound = AudioSegment.from_wav("audio_temp.wav")
-                sound.export("audio_temp.mp3", format="mp3")
-                
-                # Llamar a la función recognize_custom con el audio en formato MP3
-                product_name = recognize_custom("audio_temp.mp3")
+                # Buscar la palabra reconocida en el diccionario
+                for tecnica, reconocidas in mapeo_palabras.items():
+                    if text in reconocidas:
+                        palabra_tecnica = tecnica
+                        break
 
+                if palabra_tecnica is not None:
+                    print(f"Palabra técnica correspondiente a '{text}': {palabra_tecnica}")
+                    print(f"Producto detectado: {palabra_tecnica}")
+                    print("¿Es correcto? (sí/no)")
+                    audio = recognizer.listen(mic)
+                    response = recognizer.recognize_google(audio, language="es-ES")
+                    x  =  True
+                    while x:
+                        if "salir" in response.lower():
+                            break
+                        if "sí" in response.lower():
+                            print("Que cantidad quieres?")
+                            audio = recognizer.listen(mic)
+                            cantidad = recognizer.recognize_google(audio, language="es-ES")
+                            print(f"La cantidad es: {cantidad}")
+                            # Añadir la cantidad tambieeen!!!
+                            agregar_producto(palabra_tecnica, "products.csv", "productos_nuevos.csv")
+                            print("Producto añadido exitosamente.")
+                            x = False
+
+                        elif "no" in response.lower():
+                            pass
+                    
+                else:
+                    print(f"No se encontró una palabra técnica para '{text}'")
+
+                
                 # Comprobar si el producto es correcto
-                print(f"Producto detectado: {product_name}")
+
+                """print(f"Producto detectado: {product_name}")
                 print("¿Es correcto? (sí/no)")
                 audio = recognizer.listen(mic)
                 response = recognizer.recognize_google(audio, language="es-ES")
@@ -107,7 +140,7 @@ while True:
                         product_name = recognize_custom("audio_temp.mp3")
 
 
-
+"""
 
 
             
